a13d72d89a33ba1cd7dbbf41e986e6d93f263e9e
www7585 <30789755+www7585@users.noreply.github.com>
2024-01-15 02:09:32 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -45,7 +45,7 @@ export const getPlayerList = async (server, type, id) => {
 
 // 获取一言数据
 export const getHitokoto = async () => {
-  const res = await fetch("https://v1.hitokoto.cn");
+  const res = await fetch("https://api.7585.net.cn/yan/api.php?lx=mf&format=json");
   return await res.json();
 };
 
